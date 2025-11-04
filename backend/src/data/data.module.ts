67001339460030ffda6e2ea344963d09b69c4e18
Ivan Malyshev <ivan.malyshevv@gmail.com>
2025-11-04 22:00:25 +0300

    rev-1

--- backend/src/data/data.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { configProvider } from '../app.config.provider';
-import { dataProvider } from './data.provider';
-
-@Module({
-  providers: [configProvider, dataProvider],
-  exports: [dataProvider],
-})
-export class DataModule {}
